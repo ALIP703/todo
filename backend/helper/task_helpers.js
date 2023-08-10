26c1c6ffad16646d9fa85d312339dc56c09cdc84
muhammed ali <muhammedalip703@gmail.com>
2023-08-10 10:36:29 +0530

    getAllTasks modified and getAllPriority added

--- backend/helper/task_helpers.js
+++ backend/helper/task_helpers.js
@@ -2,10 +2,31 @@ var db = require('../config/connection/connection')
 
 module.exports = {
   // fetching all table data from taskTable
-  getTasks: () => {
+  getAllTasks: () => {
     return new Promise(async (resolve, reject) => {
       try {
-        const results = await db.promise().query('SELECT * FROM taskTable');
+        const results = await db.promise().query(`
+            SELECT
+              t.id,
+              t.heading,
+              t.description,
+              t.dateTime,
+              t.image,
+              t.createdAt,
+              p.name as priority
+            FROM taskTable AS t
+            LEFT JOIN priority AS p ON t.priorityId = p.id;
+          `);
+        resolve(results[0]); // 0 th data is table row data 
+      } catch (err) {
+        reject(err);
+      }
+    })
+  },
+  getAllPriority: () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const results = await db.promise().query('SELECT * FROM priority');
         resolve(results[0]); // 0 th data is table row data 
       } catch (err) {
         reject(err);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,15 +4,21 @@ const task_helpers = require('../helper/task_helpers');
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-    res.status(200).send('welcome to task manager api');
+  res.status(200).send('welcome to task manager api');
 });
 
 router.get('/tasks', async (req, res, next) => {
-  await task_helpers.getTasks().then((response) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.status(200).send(response);
+  await task_helpers.getAllTasks().then((response) => {
+    res.status(200).json({ tasks: response, message: 'All Tasks fetch successfully' });
   }).catch((err) => {
-    console.log('priority');
+    res.status(500).send('Error fetching Tasks: ' + err);
+  })
+});
+
+router.get('/priorities', async (req, res, next) => {
+  await task_helpers.getAllPriority().then((response) => {
+    res.status(200).json({ priorities: response, message: 'All Priorities fetch successfully' });
+  }).catch((err) => {
     res.status(500).send('Error fetching Tasks: ' + err);
   })
 });
